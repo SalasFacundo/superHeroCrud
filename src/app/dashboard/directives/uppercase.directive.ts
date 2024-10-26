26b9aaae119c8cda111a2485227204ff72e8e43f
Facundo <salasfacundo96@gmail.com>
2024-10-26 16:08:32 -0300

    Cambio constructor por inject

--- src/app/dashboard/directives/uppercase.directive.ts
+++ src/app/dashboard/directives/uppercase.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, inject } from '@angular/core';
 
 @Directive({
   selector: '[uppercase]',
@@ -6,7 +6,7 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class UppercaseDirective {
 
-  constructor(private elementRef: ElementRef) { }
+  elementRef = inject(ElementRef)
 
   @HostListener('input', ['$event'])
   onInputChange(event: Event): void {
