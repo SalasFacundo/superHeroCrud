15104d373e197292e6aca6006bcee691d462e0d2
Facundo <salasfacundo96@gmail.com>
2024-10-30 00:22:13 -0300

    Fix validacion campo en blanco

--- src/app/dashboard/components/modals/add-edit-modal/add-edit-modal.component.ts
+++ src/app/dashboard/components/modals/add-edit-modal/add-edit-modal.component.ts
@@ -16,34 +16,39 @@ import { MatInputModule } from '@angular/material/input';
 import { UppercaseDirective } from '../../../directives/uppercase.directive';
 import { SuperheroService } from '../../../services/superhero.service';
 
-const MATERIAL_MODULES = [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatDialogContent, MatDialogActions, MatDialogModule ];
+const MATERIAL_MODULES = [
+  MatFormFieldModule,
+  MatInputModule,
+  FormsModule,
+  MatButtonModule,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogModule,
+];
 
 @Component({
   selector: 'add-edit-modal',
   standalone: true,
   imports: [MATERIAL_MODULES, UppercaseDirective],
   templateUrl: './add-edit-modal.component.html',
-  styleUrl: './add-edit-modal.component.scss'
+  styleUrl: './add-edit-modal.component.scss',
 })
-
 export class AddEditModalComponent {
-
   data = inject(MAT_DIALOG_DATA);
   superheroService = inject(SuperheroService);
   readonly dialogRef = inject(MatDialogRef<AddEditModalComponent>);
   readonly name = model(this.data.superhero?.name || '');
   errorMessage = signal('');
 
-  validData(){
-    if(this.name().trim() == ""){
-      this.errorMessage.set("Field Required");
-    }
-    if(this.superheroService.getSuperheroByName(this.name())){
-      this.errorMessage.set("Superhero exists");
+  validData() {
+    if (this.name().trim() === '') {
+      this.errorMessage.set('Field Required');
+    } else if (this.superheroService.getSuperheroByName(this.name())) {
+      this.errorMessage.set('Superhero exists');
     } else {
-      this.errorMessage.set("");
+      this.errorMessage.set('');
     }
-    if(this.errorMessage() == '') {
+    if (this.errorMessage() == '') {
       this.dialogRef.close(this.name().trim());
     }
   }
